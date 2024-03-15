2ba68354b80c97a698cfe29539dc340858a699d5
RajeshEslavath <rajesheslavath2001@gmail.com>
2024-03-15 23:13:21 +0530

    update

---
+++ controlledform/src/App.jsx
@@ -0,0 +1,57 @@
+import React ,{useState}from "react";
+import "./App.css";
+
+const App = () => {
+  const [formDetails, setFormDetails] = useState({
+    username: "",
+    email: "",
+  });
+  const change=(e)=>
+  {
+    const{name,value}=e.target;
+    setFormDetails({...formDetails,[name]:value})
+  }
+  const submit=(e)=>
+  {
+    e.preventDefaults()
+    console.log(e.target.value)
+    console.log(formDetails)
+  }
+  return (
+    <div style={{padding:"100px"}}>
+      <h1>Contact Information</h1>
+      <form action="" className="myform" onSubmit={submit}>
+        <input
+          type="text"
+          name="username"
+          id="un"
+          placeholder="Enter username"
+          onChange={change}
+        />
+        <br />
+        <br />
+        <input 
+          type="email" 
+          name="email" 
+          id="em" 
+          placeholder="Entere email" 
+          onChange={change}
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="pwd"
+          placeholder="Enter password"
+          onChange={change}
+        />
+        <br />
+        <br />
+        <button type="submit" >submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
